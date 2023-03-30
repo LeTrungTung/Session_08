8cec8ee4addb8d763f86557e19f308d89cd915a2
LeTrungTung <trungtung5484@gmail.com>
2023-03-30 23:06:57 +0700

    Bài tập 8

---
+++ script.js
@@ -0,0 +1,5 @@
+let value = prompt("Nhap tuoi:");
+
+console.log("Tuoi la:", value);
+console.log("Nhap nam sinh:", prompt("Nhap nam sinh:"));
+console.log("Ban dong y yeu toi ko?:", confirm("Ban dong y khong?"));
